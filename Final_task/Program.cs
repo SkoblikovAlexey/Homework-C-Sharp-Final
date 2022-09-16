d4beae60763c0fff96f3819c2ce786809d4b56fb
Alexey Skoblikov <happycow32@yandex.ru>
2022-09-17 02:30:13 +0300

    C Sharp Code added

---
+++ Final_task/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] array = {"hello", "2", "world", ":-)"};
+int CalcNewArrayLength(string[] array)
+{
+    int length = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3) length++;
+    }
+    return length;
+}
+
+string[] NewArray(string[] array, int N)
+{
+    string[] newArray = new string[N];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if(array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        } 
+    }
+    return newArray;
+}
+int newArrayLength = CalcNewArrayLength(array); 
+
+    void PrintArray(string[] arr)
+    {
+        int length = arr.Length;
+        Console.Write("[");
+        for (int i = 0; i < length - 1; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        Console.WriteLine(arr[length - 1] + "]");
+    }
+    string[] newArray = NewArray(array, newArrayLength);
+    PrintArray(newArray);
